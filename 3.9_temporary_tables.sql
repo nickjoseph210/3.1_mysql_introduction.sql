8f4283cf75a703ece8cc82341751bd43dd587722
Nick <nickpjoseph210@gmail.com>
2020-02-14 10:00:05 -0600

    last changes before reviewing with Zach

--- 3.9_temporary_tables.sql
+++ 3.9_temporary_tables.sql
@@ -1,46 +1,27 @@
-USE curie_953;
-#1
-CREATE TEMPORARY TABLE "yetis" (
-  `emp_no` INT(11) NOT NULL,
-  `birth_date` DATE NOT NULL,
-  `first_name` VARCHAR(14) NOT NULL,
-  `last_name` VARCHAR(16) NOT NULL,
-  `gender` ENUM('M','F') NOT NULL,
-  `hire_date` DATE NOT NULL,
-  PRIMARY KEY (`emp_no`)
-) ENGINE=INNODB DEFAULT CHARSET=latin1;
+CREATE TEMPORARY TABLE employees_with_departments AS
+SELECT emp_no, first_name, last_name, dept_no, dept_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no)
+LIMIT 100;
 
-ALTER TABLE employees_with_departments ADD full_name VARCHAR(30);
+SELECT * FROM employees_with_departments;
+
+ALTER TABLE employees_with_departments INSERT full_name VARCHAR(30);
 
 ALTER TABLE employees_with_departments DROP COLUMN first_name;
-
 ALTER TABLE employees_with_departments DROP COLUMN last_name;
 
-#I could have gotten this same table also by 
+SELECT * FROM employees_with_departments;
 
-#2
 
-USE sakila; 
 
-CREATE TEMPORARY TABLE "payment" (
-  `payment_id` SMALLINT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `customer_id` SMALLINT(5) UNSIGNED NOT NULL,
-  `staff_id` TINYINT(3) UNSIGNED NOT NULL,
-  `rental_id` INT(11) DEFAULT NULL,
-  `amount` DECIMAL(5,2) NOT NULL,
-  `payment_date` DATETIME NOT NULL,
-  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`payment_id`),
-  KEY `idx_fk_staff_id` (`staff_id`),
-  KEY `idx_fk_customer_id` (`customer_id`),
-  KEY `fk_payment_rental` (`rental_id`),
-  CONSTRAINT `fk_payment_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON UPDATE CASCADE,
-  CONSTRAINT `fk_payment_rental` FOREIGN KEY (`rental_id`) REFERENCES `rental` (`rental_id`) ON DELETE SET NULL ON UPDATE CASCADE,
-  CONSTRAINT `fk_payment_staff` FOREIGN KEY (`staff_id`) REFERENCES `staff` (`staff_id`) ON UPDATE CASCADE
-) ENGINE=INNODB AUTO_INCREMENT=16050 DEFAULT CHARSET=utf8;
 
-ALTER newPayment
-	(amount INTEGER UNSIGNED NOT NULL);
+#2a
+# transform the amount column such that it is stored as an 
+# integer representing the number of cents of the payment. For example, # 1.99 should become 199
+# = update table 
+# = set amount = int(10) unsigned not null
 
 
 
